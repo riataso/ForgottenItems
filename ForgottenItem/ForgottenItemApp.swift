f141ebdb73590e0bdc8959d785af943a96de3044
riataso <rarararai0070@gmail.com>
2024-11-18 00:46:27 +0900

    Initial Commit

---
+++ ForgottenItem/ForgottenItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgottenItemApp.swift
+//  ForgottenItem
+//
+//  Created by 佐藤来 on 2024/11/18.
+//
+
+import SwiftUI
+
+@main
+struct ForgottenItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForgottenItemTests/ForgottenItemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForgottenItemTests.swift
+//  ForgottenItemTests
+//
+//  Created by 佐藤来 on 2024/11/18.
+//
+
+import Testing
+@testable import ForgottenItem
+
+struct ForgottenItemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
