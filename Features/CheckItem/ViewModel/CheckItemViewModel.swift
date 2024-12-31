29f8d93fe5323e4d017887c8bf51f6926a24f6a1
riataso <rarararai0070@gmail.com>
2025-01-01 06:55:19 +0900

    チェック項目の追加・更新・削除処理動作を永続化処理に対応させた

--- Features/CheckItem/ViewModel/CheckItemViewModel.swift
+++ Features/CheckItem/ViewModel/CheckItemViewModel.swift
@@ -1,29 +1,59 @@
 import Foundation
+import SwiftData
 
+@MainActor
 class CheckItemViewModel: ObservableObject {
     @Published var checkItemList: [CheckItem] = []
-    @Published var editItemId: UUID?
+    @Published var inputItemName = ""
+    @Published var editItem: CheckItem?
+    private let repository: CheckItemRepotitory
 
-    func createCheckItem() {
-        editFinish()
-        let checkItem: CheckItem = .init(id: UUID(), itemName: "", checked: true)
-        checkItemList += [checkItem]
-        editItemId = checkItem.id
+    init(repository: CheckItemRepotitory) {
+        self.repository = repository
     }
 
-    func rowRemove(offsets: IndexSet) {
-        checkItemList.remove(atOffsets: offsets)
+    //チェック項目の追加用処理
+    func createCheckItem() async {
+        repository.create(itemName: inputItemName)
+        clearInputItemName()
+        checkItemList = repository.fetchAll()
     }
 
-    func editCheckItem(id: UUID, itemName: String) {
-        if let index = checkItemList.firstIndex(where: { $0.id == id }) {
-            checkItemList[index].itemName = itemName
-        }
+    //チェック項目の編集用処理
+    func editCheckItemName() async {
+        guard let editItem else { return }
+        await repository.editItem(targetId: editItem.id, updataName: inputItemName, updataChecked: editItem.checked)
+        clearInputItemName()
     }
 
-    func editFinish() {
-        editItemId = nil
-        checkItemList.removeAll { $0.itemName.trimmingCharacters(in: .whitespaces).isEmpty }
+    //チェック項目のステータス更新用処理
+    func updateCheckStatus(for item: CheckItem) async {
+        await repository.editStatus(item: item, newCheckedStatus: item.checked)
     }
 
+    //チェック項目の一覧を取得
+    func getCheckItems() async {
+        checkItemList = repository.fetchAll()
+    }
+
+    //チェック項目の削除用処理
+    func deleteCheckItem() async {
+        guard let editItem else { return }
+        try? checkItemList =  await repository.delete(targetId: editItem.id)
+    }
+
+    //入力用変数の初期化処理
+    func clearInputItemName() {
+        inputItemName = ""
+    }
+    //チェック項目作成用タイトル入力欄判定処理
+    var isButtonEnable: Bool {
+        inputItemName.trimmingCharacters(in: .whitespaces) == ""
+    }
+
+    //チェック項目編集用入力欄判定処理
+    var isEditButtonEnable: Bool {
+        let editItemData = editItem!
+        return inputItemName.trimmingCharacters(in: .whitespaces) == editItemData.itemName || inputItemName.trimmingCharacters(in: .whitespaces) == ""
+    }
 }
